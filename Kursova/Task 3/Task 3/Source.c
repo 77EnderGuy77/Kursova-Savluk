260262b4d93fd9c416eab4cba41bf1d760ca9a03
77EnderGuy77 <134065941+77EnderGuy77@users.noreply.github.com>
2023-05-31 01:28:39 +0300

    Task 3 v2.1

--- Kursova/Task 3/Task 3/Source.c
+++ Kursova/Task 3/Task 3/Source.c
@@ -1,63 +1,39 @@
 #include <stdio.h>
+#include <string.h>
 
-unsigned int pack_char(char ch1, char ch2) {
-    unsigned int result = 0;
-    result |= ch1;
-    result <<= 8;
-    result |= ch2;
-    return result;
+unsigned int pack_char(char c1, char c2) {
+    unsigned int packed = 0;
+    packed |= (unsigned int)(c1 & 0xFF);
+    packed <<= 8;
+    packed |= (unsigned int)(c2 & 0xFF);
+    return packed;
 }
 
 void print_binary(unsigned int num) {
-    unsigned int mask = 1 << (sizeof(unsigned int) * 8 - 1);
-    while (mask > 0) {
-        if (num & mask)
-            printf("1");
-        else
-            printf("0");
-        mask >>= 1;
+    for (int i = 31; i >= 0; i--) {
+        unsigned int mask = 1u << i;
+        putchar((num & mask) ? '1' : '0');
+        if (i % 8 == 0)
+            putchar(' ');
     }
-    printf("\n");
 }
 
-int main(char *argv[], int argc) {
-    if (argc < 3) {
-        printf("Please provide two characters as command line arguments.\n");
+int main(int argc, char* argv[]) {
+    if (argc != 3) {
+        printf("Usage: %s <char1> <char2>\n", argv[0]);
         return 1;
     }
 
-    char ch1 = argv[1][0];
-    char ch2 = argv[2][0];
+    char c1 = argv[1][0];
+    char c2 = argv[2][0];
 
-    unsigned int packed = pack_char(ch1, ch2);
+    unsigned int packed = pack_char(c1, c2);
 
-    printf("Character 1: %c\n", ch1);
-    printf("Character 2: %c\n", ch2);
-    printf("Result (Decimal): %u\n", packed);
-    printf("Result (Binary): ");
+    printf("Initial characters: %c %c\n", c1, c2);
+    printf("Decimal representation: %u\n", packed);
+    printf("Binary representation: ");
     print_binary(packed);
-
-    FILE *outputFile = fopen("output.txt", "w");
-    if (outputFile == NULL) {
-        printf("Error opening output file.\n");
-        return 1;
-    }
-
-    fprintf(outputFile, "Character 1: %c\n", ch1);
-    fprintf(outputFile, "Character 2: %c\n", ch2);
-    fprintf(outputFile, "Result (Decimal): %u\n", packed);
-    fprintf(outputFile, "Result (Binary): ");
-    unsigned int mask = 1 << (sizeof(unsigned int) * 8 - 1);
-    while (mask > 0) {
-        if (packed & mask)
-            fprintf(outputFile, "1");
-        else
-            fprintf(outputFile, "0");
-        mask = mask >> 1;
-    }
-    fprintf(outputFile, "\n");
-
-    fclose(outputFile);
+    printf("\n");
 
     return 0;
 }
