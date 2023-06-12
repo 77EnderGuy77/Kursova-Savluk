9f702e0f062a6399e40c10e90db18e043c718180
77EnderGuy77 <134065941+77EnderGuy77@users.noreply.github.com>
2023-06-12 11:00:02 +0300

    Task 2 v2.4

--- Kursova/Task 2/Task 2/Source.c
+++ Kursova/Task 2/Task 2/Source.c
@@ -4,10 +4,10 @@
 
 #define size 100
 
-void manualInput(); //ability to input N x N matrix by hand
-void randomInput(); //ability to randomly input N x N matrix
-void formVector(); //Form vector and shows difference in highest and lowest value by module
-void displayArray(); //Show full array 
+void manualInput(); // Function to manually input N x N matrix
+void randomInput(); // Function to randomly input N x N matrix
+void formVector(); // Function to form a vector and calculate the difference between the highest and lowest values in each row
+void displayArray(); // Function to display the full array
 
 int main() {
     int array[size][size];
@@ -18,22 +18,24 @@ int main() {
     printf("Enter the size of the matrix (6 <= N <= 10): ");
     scanf_s("%d", &N);
 
-    if (N < 6 || N > 10) { //Fail check
+    if (N < 6 || N > 10) { // Check for valid size
         printf("Invalid size....");
         return 1;
     }
 
-    printf("Choose type of input ( 1- Manual, 2 - Random): ");
+    printf("Choose the type of input (1 - Manual, 2 - Random): ");
     scanf_s("%d", &ch);
-    
-    switch (ch) //option of input
-    {
-    case 1:manualInput(array, N);
-        break;
-    case 2:randomInput(array, N);
-        break;
-    default:printf("Invalid option.");
-        return 1;
+
+    switch (ch) {
+        case 1:
+            manualInput(array, N);
+            break;
+        case 2:
+            randomInput(array, N);
+            break;
+        default:
+            printf("Invalid option.");
+            return 1;
     }
 
     displayArray(array, N, ch);
@@ -44,37 +46,40 @@ int main() {
 }
 
 void manualInput(int array[size][size], int N) {
-
     printf("Input array manually: \n");
-    for (int i = 0; i < N; i++)
-    {
-        for (int j = 0; j < N; j++)
-        {
-            scanf_s("%d", &array[i][j]); //Manual input of array
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            scanf_s("%d", &array[i][j]); // Manually input array elements
         }
     }   
 }
 
 void randomInput(int array[size][size], int N) {
-    
     for (int i = 0; i < N; i++) {
         for (int j = 0; j < N; j++) {
-            array[i][j] = rand() % 201 - 100; // random fill of array from -100 to 200
+            array[i][j] = rand() % 201 - 100; // Randomly fill array with values from -100 to 200
         }
     }
 }
 
 void displayArray(int array[size][size], int N, int ch) {
-    switch (ch) //Just to make sure what type of fill
-    {
-    case 1: printf("Manually inputed matrix: \n");
-        break;
-    case 2:printf("Randomly inputed matrix: \n");
-        break;
+    switch (ch) {
+        case 1:
+            printf("\tManually inputted matrix: \n");
+            break;
+        case 2:
+            printf("\tRandomly inputted matrix: \n");
+            break;
     }
-    for (int i = 0; i < N; i++){ //print of array  
-        for (int j = 0; j < N; j++){
-            printf("%d | ", array[i][j]);
+        printf("   | ");
+    for (int g = 1; g <= N; g++) {
+        printf("%3d | ", g);
+    }
+        printf("\n");
+    for (int i = 0; i < N; i++) {
+        printf("%2d | ", i + 1);
+        for (int j = 0; j < N; j++) {
+            printf("%3d | ", array[i][j]); // Print array elements
         }
         printf("\n");
     }
@@ -83,12 +88,10 @@ void displayArray(int array[size][size], int N, int ch) {
 void formVector(int array[size][size], int N) {
     int diff[size];
 
-    for (int i = 0; i < N; i++)
-    {
+    for (int i = 0; i < N; i++) {
         int min = array[i][0];
         int max = array[i][0];
-        for (int j = 0; j < N; j++)
-        {
+        for (int j = 0; j < N; j++) {
             if (array[i][j] < min) {
                 min = array[i][j];
             }
@@ -96,14 +99,14 @@ void formVector(int array[size][size], int N) {
                 max = array[i][j];
             }
         }
-        diff[i] = max - min;
+        diff[i] = max - min; // Calculate the difference between the highest and lowest values in each row
     }
 
     printf("Vector of differences of the largest and smallest values in each row:\n");
 
-    printf("Value:");//print values of rows 
+    printf("Value:"); // Print the values of the vector
     for (int i = 0; i < N; i++) {
-        printf("%d ", diff[i]);
+        printf("Row [%d]: %d, ", (i + 1), diff[i]);
     }
 
     printf("\n");
